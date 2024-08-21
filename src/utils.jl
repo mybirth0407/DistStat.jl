cec7b0534c633c1d57a209778ff8273aa84c610d
Yedarm Seong <mybirth0407@gmail.com>
2024-08-22 02:21:26 +0900

    add npyread

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,9 @@
 import LinearAlgebra: diagind
+import NPZ
+import MPI
+import MPI: COMM_WORLD
+export npyread
+
 """
     euclidean_distance(out, A, B; tmp_m=nothing, tmp_n=nothing)
 
@@ -32,6 +37,7 @@ function euclidean_distance!(out::AbstractArray, A::AbstractMatrix, B::AbstractM
     end
     out
 end
+
 
 """
     euclidean_distance!(out, A)
@@ -80,5 +86,61 @@ function euclidean_distance!(out::MPIMatrix{T,A}, data::MPIMatrix{T,A};
                             tmp_m = @view(tmp_vec2[1:tmp_dist_cols]))
         out.localarray[out.partitioning[r+1][2], :] .= tmp_dist_view
     end
-    out 
+    out
+end
+
+
+function npyread(
+    filename::AbstractString;
+    root=0, A=Array, group_info::Vector{Int}=Int[]
+)
+    success = 0
+    shape = 0
+    T = nothing
+    hdrend = 0
+    toh = nothing
+
+    if Rank() == root
+        f = open(filename, "r")
+        b = read!(f, Vector{UInt8}(undef, NPZ.MaxMagicLen))
+        if NPZ.samestart(b, NPZ.NPYMagic)
+            seekstart(f)
+            hdr = NPZ.readheader(f)
+            if hdr.fortran_order
+                shape = hdr.shape
+                T = eltype(hdr)
+                toh = hdr.descr
+                hdrend = mark(f)
+
+                success = 1
+                close(f)
+            else
+                success = -1
+                close(f)
+            end
+        else
+            close(f)
+        end
+    end
+    success = MPI.bcast(success, root, MPI.COMM_WORLD)
+    if success == 0
+        error("not a NPY file supported: $filename")
+    end
+    if success == -1
+        error("NPY file must be in fortran order: $filename")
+    end
+    shape, T, hdrend, toh = MPI.bcast(
+        (shape, T, hdrend, toh), root, MPI.COMM_WORLD
+    )
+    rslt = MPIArray{T, length(shape), A}(undef, shape...)
+
+    arr_skip = sum(rslt.local_lengths[1:Rank()])
+    println(hdrend + arr_skip * sizeof(T))
+    f = open(filename, "r")
+    seek(f, hdrend + arr_skip * sizeof(T))
+    rslt.localarray = map(
+        toh, read!(f, Array{T}(undef, size(rslt.localarray)))
+    )
+    close(f)
+    rslt
 end
